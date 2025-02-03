454d716f447dc0ab65d6c7e112e67b0058f3a7cc
grideperlevy <mr.ivandmi@yandex.ru>
2025-02-03 22:01:31 +0300

    ошибки и оформление кода

--- movie_api_task/student_solution.py
+++ movie_api_task/student_solution.py
@@ -6,11 +6,12 @@ import elasticsearch as ES
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return 'worked'
+    return "worked"
 
-@app.route('/api/movies/')
+
+@app.route("/api/movies/")
 def movie_list():
     # validate = validate_args(request.args)
     #
@@ -18,10 +19,10 @@ def movie_list():
     #     return abort(422)
 
     defaults = {
-        'limit': 50,
-        'page': 1,
-        'sort': 'id',
-        'sort_order': 'asc'
+        "limit": 50,
+        "page": 1,
+        "sort": "id",
+        "sort_order": "asc"
     }
 
     # Тут уже валидно все
@@ -32,53 +33,59 @@ def movie_list():
     body = {
         "query": {
             "multi_match": {
-                "query": defaults['search'],
-                "fields": ["title"]
+                "query": defaults["search"],
+                "fields": ["title", "description", "genre", "actors_names", "writers_names", "director"]
             }
         }
-    } if defaults.get('search', False) else {}
+    } if defaults.get("search", False) else {}
 
-    body['_source'] = dict()
-    body['_source']['include'] = ['id', 'title', 'imdb_rating']
+    body["_source"] = dict()
+    body["_source"]["includes"] = ["title", "description", "genre", "actors_names", "writers_names", "director"]
 
     params = {
         # '_source': ['id', 'title', 'imdb_rating'],
-        'from': int(defaults['limit']) * (int(defaults['page']) - 1),
-        'size': defaults['limit'],
-        'sort': [
+        "from": int(defaults["limit"]) * (int(defaults["page"]) - 1),
+        "size": defaults["limit"],
+        "sort": [
             {
                 defaults["sort"]: defaults["sort_order"]
             }
         ]
     }
 
-    es_client = ES.Elasticsearch([{'host': '192.168.11.128', 'port': 9200}], )
+    es_client = ES.Elasticsearch([{"host": "192.168.11.128", "port": 9200}], )
     search_res = es_client.search(
         body=body,
-        index='movies',
+        index="movies",
         params=params,
-        filter_path=['hits.hits._source']
+        filter_path=["hits.hits._source"]
     )
     es_client.close()
 
-    return jsonify([doc['_source'] for doc in search_res['hits']['hits']])
+    return jsonify([doc["_source"] for doc in search_res["hits"]["hits"]])
 
 
-@app.route('/api/movies/<string:movie_id>')
+@app.route("/api/movies/<string:movie_id>")
 def get_movie(movie_id):
-    es_client = ES.Elasticsearch([{'host': '192.168.11.128', 'port': 9200}], )
+    es_client = ES.Elasticsearch([{"host": "192.168.11.128", "port": 9200}], )
 
-    if not es_client.ping():
-        print('oh(')
+    try:
+        if not es_client.ping():
+            return jsonify({"error":"oh("}), 500
 
-    search_result = es_client.get(index='movies', id=movie_id, ignore=404)
+        search_result = es_client.get(index="movies", id=movie_id, ignore=404)
 
-    es_client.close()
+        if search_result and search_result.get("found", False):
+            return jsonify(search_result["_source"])
 
-    if search_result['found']:
-        return jsonify(search_result['_source'])
+        return jsonify({"error": "Movie not found"}), 404
 
-    return abort(404)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+    finally:
+        es_client.close()
+
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8000)
+    app.run(host="127.0.0.1", port=8000)
